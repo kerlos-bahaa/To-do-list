97c70db19ccba0c5ee1557fbebb8504ba5b54433
kerlosbahaa3 <aboelking4@gmail.com>
2023-06-08 04:20:29 +0300

    Finish

---
+++ src/index.js
@@ -0,0 +1,46 @@
+import './style.css';
+
+const todos = [
+  {
+    index: 1,
+    description: 'Clean My Room',
+    completed: true,
+  },
+  {
+    index: 2,
+    description: 'Do homework',
+    completed: false,
+  },
+  {
+    index: 3,
+    description: 'Buy groceries',
+    completed: false,
+  },
+  {
+    index: 4,
+    description: 'pray',
+    completed: false,
+  },
+  {
+    index: 5,
+    description: 'Go for a run',
+    completed: false,
+  },
+];
+/* eslint-disable no-console */
+document.addEventListener('DOMContentLoaded', showList);
+
+function showList() {
+  const taskList = document.getElementById('todo-list');
+  taskList.innerHTML = '';
+  todos.sort((a, b) => a.index - b.index);
+
+  todos.forEach((todo) => {
+    const item = document.createElement('li');
+    item.textContent = todo.description;
+    if (todo.completed) {
+      item.classList.add('completed');
+    }
+    taskList.appendChild(item);
+  });
+}
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, './src/index.js'),
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
+  ],
+
+};
