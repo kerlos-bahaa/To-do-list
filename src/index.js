87369f43987f44db137dec1d0700eab95f559cdc
kerlosbahaa3 <aboelking4@gmail.com>
2023-06-08 04:37:40 +0300

    Last fix

--- src/index.js
+++ src/index.js
@@ -27,20 +27,19 @@ const todos = [
     completed: false,
   },
 ];
-/* eslint-disable */
-document.addEventListener("DOMContentLoaded", showList);
 
 function showList() {
-  const taskList = document.getElementById("todo-list");
-  taskList.innerHTML = "";
+  const taskList = document.getElementById('todo-list');
+  taskList.innerHTML = '';
   todos.sort((a, b) => a.index - b.index);
 
   todos.forEach((todo) => {
-    const item = document.createElement("li");
+    const item = document.createElement('li');
     item.textContent = todo.description;
     if (todo.completed) {
-      item.classList.add("completed");
+      item.classList.add('completed');
     }
     taskList.appendChild(item);
   });
 }
+document.addEventListener('DOMContentLoaded', showList);
