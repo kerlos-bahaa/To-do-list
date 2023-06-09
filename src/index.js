d8e7792f2ec2fb32009fab73e9ea18c6dbb0bf74
kerlosbahaa3 <aboelking4@gmail.com>
2023-06-09 16:21:23 +0300

    Linter

--- src/index.js
+++ src/index.js
@@ -1,72 +1,69 @@
-import './style.css'
-import { todos, addTask, deleteTask, editTask, loadTasks } from './todo';
+import './style.css';
+import {
+  todos, addTask, deleteTask, editTask, loadTasks,
+} from './todo.js';
 
 // console.log(todos);
 
-
 function showList() {
-    loadTasks();
+  loadTasks();
 
-    const taskList = document.getElementById('todo-list');
+  const taskList = document.getElementById('todo-list');
 
-    taskList.innerHTML = '';
+  taskList.innerHTML = '';
 
-    todos.sort((a, b) => a.index - b.index);
+  todos.sort((a, b) => a.index - b.index);
 
+  todos.forEach((todo, index) => {
+    const item = document.createElement('li');
+    const itemDiv = document.createElement('div');
+    const itemSpan = document.createElement('span');
+    itemDiv.className = 'task-item';
+    itemSpan.textContent = todo.description;
+    itemSpan.className = todo.completed ? 'completed' : '';
+    itemSpan.dataset.index = index;
+    itemSpan.setAttribute('contenteditable', 'true');
+    itemDiv.appendChild(itemSpan);
 
-    todos.forEach((todo, index) => {
-        const item = document.createElement('li');
-        const itemDiv = document.createElement('div');
-        const itemSpan = document.createElement('span');
-        itemDiv.className = 'task-item';
-        itemSpan.textContent = todo.description;
-        itemSpan.className = todo.completed ? "completed" : '';
-        itemSpan.dataset.index = index;
-        itemSpan.setAttribute("contenteditable", "true");
-        itemDiv.appendChild(itemSpan);
+    const deleteButton = document.createElement('i');
+    deleteButton.className = 'fas fa-trash-alt text-danger float-end delete-button';
 
-        const deleteButton = document.createElement("i");
-        deleteButton.className = "fas fa-trash-alt text-danger float-end delete-button";
-
-
-        item.appendChild(itemDiv);
-        item.appendChild(deleteButton);
-        taskList.appendChild(item);
-    });
-
+    item.appendChild(itemDiv);
+    item.appendChild(deleteButton);
+    taskList.appendChild(item);
+  });
 }
 
-document.getElementById("add-task-form").addEventListener("submit", event => {
-    event.preventDefault();
-    const taskDescription = document.getElementById("task-input").value;
-    addTask(taskDescription);
+document.getElementById('add-task-form').addEventListener('submit', (event) => {
+  event.preventDefault();
+  const taskDescription = document.getElementById('task-input').value;
+  addTask(taskDescription);
+  showList();
+  document.getElementById('task-input').value = '';
+});
+
+document.addEventListener('click', (event) => {
+  if (event.target.classList.contains('delete-button')) {
+    const listItem = event.target.parentNode;
+    const index = parseInt(listItem.dataset.index);
+    deleteTask(index);
+    listItem.remove();
     showList();
-    document.getElementById("task-input").value = "";
+  }
 });
 
-document.addEventListener("click", function (event) {
-    if (event.target.classList.contains("delete-button")) {
-        const listItem = event.target.parentNode;
-        const index = parseInt(listItem.dataset.index);
-        deleteTask(index);
-        listItem.remove();
-        showList();
-    }
+document.addEventListener('input', (event) => {
+  if (event.target.tagName === 'SPAN') {
+    const listItem = event.target;
+    const index = parseInt(listItem.dataset.index);
+    const newDescription = listItem.textContent.trim();
+    editTask(index, newDescription);
+  }
 });
 
-
-document.addEventListener('input', function (event) {
-    if (event.target.tagName === 'SPAN') {
-        const listItem = event.target;
-        const index = parseInt(listItem.dataset.index);
-        const newDescription = listItem.textContent.trim();
-        editTask(index, newDescription);
-    }
-});
-
-document.addEventListener('blur', function (event) {
-    if (event.target.tagName === 'SPAN') {
-        showList();
-    }
+document.addEventListener('blur', (event) => {
+  if (event.target.tagName === 'SPAN') {
+    showList();
+  }
 });
 document.addEventListener('DOMContentLoaded', showList);
--- webpack.config.js
+++ webpack.config.js
@@ -3,39 +3,37 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-
 module.exports = {
-    entry: {
-        main: path.resolve(__dirname, "./src/index.js")
-    },
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.html$/i,
-                loader: 'html-loader',
-            },
-            {
-                test: /\.css$/i,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    "css-loader",
-                ],
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: './src/index.html'
-        }),
-        new MiniCssExtractPlugin({
-            filename: "style.css",
-        }),
+  entry: {
+    main: path.resolve(__dirname, './src/index.js'),
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+        ],
+      },
     ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
+  ],
 
-
-}
+};
