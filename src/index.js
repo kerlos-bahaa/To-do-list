5a7c3309c76ced75cda495eb5e13b6a0d2b76dd4
kerlosbahaa3 <aboelking4@gmail.com>
2023-06-09 20:38:49 +0300

    Finish

--- src/index.js
+++ src/index.js
@@ -1,45 +1,69 @@
 import './style.css';
+import {
+  todos, addTask, deleteTask, editTask, loadTasks,
+} from './todo';
 
-const todos = [
-  {
-    index: 1,
-    description: 'Clean My Room',
-    completed: true,
-  },
-  {
-    index: 2,
-    description: 'Do homework',
-    completed: false,
-  },
-  {
-    index: 3,
-    description: 'Buy groceries',
-    completed: false,
-  },
-  {
-    index: 4,
-    description: 'pray',
-    completed: false,
-  },
-  {
-    index: 5,
-    description: 'Go for a run',
-    completed: false,
-  },
-];
+// console.log(todos);
 
 function showList() {
+  loadTasks();
+
   const taskList = document.getElementById('todo-list');
+
   taskList.innerHTML = '';
+
   todos.sort((a, b) => a.index - b.index);
 
-  todos.forEach((todo) => {
+  todos.forEach((todo, index) => {
     const item = document.createElement('li');
-    item.textContent = todo.description;
-    if (todo.completed) {
-      item.classList.add('completed');
-    }
+    const itemDiv = document.createElement('div');
+    const itemSpan = document.createElement('span');
+    itemDiv.className = 'task-item';
+    itemSpan.textContent = todo.description;
+    itemSpan.className = todo.completed ? 'completed' : '';
+    itemSpan.dataset.index = index;
+    itemSpan.setAttribute('contenteditable', 'true');
+    itemDiv.appendChild(itemSpan);
+
+    const deleteButton = document.createElement('i');
+    deleteButton.className = 'fas fa-trash-alt text-danger float-end delete-button';
+
+    item.appendChild(itemDiv);
+    item.appendChild(deleteButton);
     taskList.appendChild(item);
   });
 }
+
+document.getElementById('add-task-form').addEventListener('submit', (event) => {
+  event.preventDefault();
+  const taskDescription = document.getElementById('task-input').value;
+  addTask(taskDescription);
+  showList();
+  document.getElementById('task-input').value = '';
+});
+
+document.addEventListener('click', (event) => {
+  if (event.target.classList.contains('delete-button')) {
+    const listItem = event.target.parentNode;
+    const index = parseInt(listItem.dataset.index);
+    deleteTask(index);
+    listItem.remove();
+    showList();
+  }
+});
+
+document.addEventListener('input', (event) => {
+  if (event.target.tagName === 'SPAN') {
+    const listItem = event.target;
+    const index = parseInt(listItem.dataset.index);
+    const newDescription = listItem.textContent.trim();
+    editTask(index, newDescription);
+  }
+});
+
+document.addEventListener('blur', (event) => {
+  if (event.target.tagName === 'SPAN') {
+    showList();
+  }
+});
 document.addEventListener('DOMContentLoaded', showList);
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: {
