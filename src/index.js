71c845575c0ac4479f3116bd798f215a7b41dd94
kerlosbahaa3 <aboelking4@gmail.com>
2023-06-09 20:21:07 +0300

    Linter

--- src/index.js
+++ src/index.js
@@ -1,32 +1,31 @@
-import './style.css';
-import {
-  todos, addTask, deleteTask, editTask, loadTasks,
-} from './todo.js';
+import "./style.css";
+import { todos, addTask, deleteTask, editTask, loadTasks } from "./todo.js";
 
 // console.log(todos);
 
 function showList() {
   loadTasks();
 
-  const taskList = document.getElementById('todo-list');
+  const taskList = document.getElementById("todo-list");
 
-  taskList.innerHTML = '';
+  taskList.innerHTML = "";
 
   todos.sort((a, b) => a.index - b.index);
 
   todos.forEach((todo, index) => {
-    const item = document.createElement('li');
-    const itemDiv = document.createElement('div');
-    const itemSpan = document.createElement('span');
-    itemDiv.className = 'task-item';
+    const item = document.createElement("li");
+    const itemDiv = document.createElement("div");
+    const itemSpan = document.createElement("span");
+    itemDiv.className = "task-item";
     itemSpan.textContent = todo.description;
-    itemSpan.className = todo.completed ? 'completed' : '';
+    itemSpan.className = todo.completed ? "completed" : "";
     itemSpan.dataset.index = index;
-    itemSpan.setAttribute('contenteditable', 'true');
+    itemSpan.setAttribute("contenteditable", "true");
     itemDiv.appendChild(itemSpan);
 
-    const deleteButton = document.createElement('i');
-    deleteButton.className = 'fas fa-trash-alt text-danger float-end delete-button';
+    const deleteButton = document.createElement("i");
+    deleteButton.className =
+      "fas fa-trash-alt text-danger float-end delete-button";
 
     item.appendChild(itemDiv);
     item.appendChild(deleteButton);
@@ -34,16 +33,16 @@ function showList() {
   });
 }
 
-document.getElementById('add-task-form').addEventListener('submit', (event) => {
+document.getElementById("add-task-form").addEventListener("submit", (event) => {
   event.preventDefault();
-  const taskDescription = document.getElementById('task-input').value;
+  const taskDescription = document.getElementById("task-input").value;
   addTask(taskDescription);
   showList();
-  document.getElementById('task-input').value = '';
+  document.getElementById("task-input").value = "";
 });
 
-document.addEventListener('click', (event) => {
-  if (event.target.classList.contains('delete-button')) {
+document.addEventListener("click", (event) => {
+  if (event.target.classList.contains("delete-button")) {
     const listItem = event.target.parentNode;
     const index = parseInt(listItem.dataset.index);
     deleteTask(index);
@@ -52,8 +51,8 @@ document.addEventListener('click', (event) => {
   }
 });
 
-document.addEventListener('input', (event) => {
-  if (event.target.tagName === 'SPAN') {
+document.addEventListener("input", (event) => {
+  if (event.target.tagName === "SPAN") {
     const listItem = event.target;
     const index = parseInt(listItem.dataset.index);
     const newDescription = listItem.textContent.trim();
@@ -61,9 +60,9 @@ document.addEventListener('input', (event) => {
   }
 });
 
-document.addEventListener('blur', (event) => {
-  if (event.target.tagName === 'SPAN') {
+document.addEventListener("blur", (event) => {
+  if (event.target.tagName === "SPAN") {
     showList();
   }
 });
-document.addEventListener('DOMContentLoaded', showList);
+document.addEventListener("DOMContentLoaded", showList);
